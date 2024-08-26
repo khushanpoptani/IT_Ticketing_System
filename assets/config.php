d3eac36c92c1e2389e264b0bbe153cac2b6e3a9b
Khushan Poptani <69676049+khushanpoptani@users.noreply.github.com>
2024-08-26 23:48:40 +0530

    Update config.php

--- assets/config.php
+++ assets/config.php
@@ -1,10 +1,10 @@
-<?php
+ame<?php
 // assets/config.php
 
 define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'ABCD1234');
-define('DB_DATABASE', 'ticketing');
+define('DB_USERNAME', 'username');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'dbname');
 
 function getDB() {
     $dbConnection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
